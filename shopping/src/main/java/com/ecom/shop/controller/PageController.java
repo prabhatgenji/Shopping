56655f044f4592cbbe057832d2ff7cdfec517386
CLASSIC <CLASSIC@GENJI>
2017-12-08 15:03:22 +0530

    successfully loaded static resources

---
+++ shopping/src/main/java/com/ecom/shop/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.ecom.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+@RequestMapping(value= {"/", "/home", "/index"})
+public ModelAndView index() {
+	ModelAndView mv=new ModelAndView("page");
+	mv.addObject("greeting", "welcome to web mvc");
+	return mv;
+ }
+ @RequestMapping(value="/test")
+public ModelAndView test(@RequestParam("greeting") String greeting) {
+	 ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv; 	
+}
+}
